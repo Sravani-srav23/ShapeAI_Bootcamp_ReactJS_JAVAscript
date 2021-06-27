7694404f166eeaa645e86d4131339860ef6c784a
Sravani-srav23 <86474644+Sravani-srav23@users.noreply.github.com>
2021-06-27 11:40:38 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>JAVAScript and React.JS</h1>
+      <p>A basic web dev React JS Boot camp</p>
+    </div>
+  );
+}
+export default Info;
